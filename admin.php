1137516a39696bdf67cc512a92332896d5b8cad4
Leandro Ocarol <ocaroleandro@gmail.com>
2026-03-24 15:55:56 +0800

    My local changes

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CCS Sit-in Monitoring System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <span class="nav-brand">Dashboard</span>
+        <ul class="nav-links">
+            <li class="dropdown"><a href="#">Community ▾</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="editProfile.php">Edit Profile</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">Reservation</a></li>
+            <li><a href="logout.php">LOG OUT</a></li>
+        </ul>
+    </nav>
+
+    
+</body>
+</html>
