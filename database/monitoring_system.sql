416853e01615af632b990c7ce568695efb9d6483
unknown <TT228@ccs.local>
2026-03-26 11:45:20 +0800

    03/26/2026(V2)

--- database/monitoring_system.sql
+++ database/monitoring_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2026 at 05:11 PM
+-- Generation Time: Mar 26, 2026 at 04:43 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,6 +45,9 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `last_name`, `first_name`, `middle_name`, `student_password`, `course`, `course_level`, `email`, `address`, `profile_image`) VALUES
+(1234, 'oracxol', 'leandro', 's', '$2y$10$.juy2dAS3cAriI2y6YBLDu04WhD89OJJZhNQOsGVbTNWeeUKgodea', 'BEED', 3, 'orakol@gmail.com', 'ormoc', 'IMG_1234.jpg'),
+(12345, 'Sermonia', 'John Carlo ', 'Dominguez', '$2y$10$0W6a/c6UxghdaNhSF7P0F.d16Qdqtz5c4TzWFSsLhfgFkcCpWhVK6', 'BSIT', 3, 'carlosermonia14@gmail.com', 'Lapu Lapu', 'IMG_Default.jpg'),
+(23792344, 'Aranez', 'AxelJake', '', '$2y$10$Qvs.CxpVDe4QyN8vcas/FuUl56KALBLLmBfDuHdl1b0WBzuVGjcq2', 'BSIT', 3, 'axeljake@gmail.com', 'lahug', 'IMG_Default.jpg'),
 (23798606, 'Ocarol', 'Leandro', 'Cabalda', '$2y$10$hPMpSW2uG8xLSR/D60ALFOo0dE3Uv2UXr4dWb6PrneeTdzHdq2zSy', 'BSIT', 3, 'ocaroleandro@gmail.com', 'Pasil, Cebu City', 'IMG_23798606.jpg');
 
 --
