e9481174e60061d6c531702be652168c350c22cf
unknown <TT228@ccs.local>
2026-03-26 16:15:57 +0800

    DONE CHECKING

--- database/monitoring_system.sql
+++ database/monitoring_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2026 at 04:43 AM
+-- Generation Time: Mar 26, 2026 at 09:15 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,18 +37,20 @@ CREATE TABLE `students` (
   `course_level` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `address` varchar(75) NOT NULL,
-  `profile_image` varchar(100) DEFAULT 'IMG_Default.jpg'
+  `profile_image` varchar(100) DEFAULT 'IMG_Default.jpg',
+  `Session` int(11) DEFAULT 30
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `last_name`, `first_name`, `middle_name`, `student_password`, `course`, `course_level`, `email`, `address`, `profile_image`) VALUES
-(1234, 'oracxol', 'leandro', 's', '$2y$10$.juy2dAS3cAriI2y6YBLDu04WhD89OJJZhNQOsGVbTNWeeUKgodea', 'BEED', 3, 'orakol@gmail.com', 'ormoc', 'IMG_1234.jpg'),
-(12345, 'Sermonia', 'John Carlo ', 'Dominguez', '$2y$10$0W6a/c6UxghdaNhSF7P0F.d16Qdqtz5c4TzWFSsLhfgFkcCpWhVK6', 'BSIT', 3, 'carlosermonia14@gmail.com', 'Lapu Lapu', 'IMG_Default.jpg'),
-(23792344, 'Aranez', 'AxelJake', '', '$2y$10$Qvs.CxpVDe4QyN8vcas/FuUl56KALBLLmBfDuHdl1b0WBzuVGjcq2', 'BSIT', 3, 'axeljake@gmail.com', 'lahug', 'IMG_Default.jpg'),
-(23798606, 'Ocarol', 'Leandro', 'Cabalda', '$2y$10$hPMpSW2uG8xLSR/D60ALFOo0dE3Uv2UXr4dWb6PrneeTdzHdq2zSy', 'BSIT', 3, 'ocaroleandro@gmail.com', 'Pasil, Cebu City', 'IMG_23798606.jpg');
+INSERT INTO `students` (`id`, `last_name`, `first_name`, `middle_name`, `student_password`, `course`, `course_level`, `email`, `address`, `profile_image`, `Session`) VALUES
+(1234, 'oracxol', 'leandro', 's', '$2y$10$.juy2dAS3cAriI2y6YBLDu04WhD89OJJZhNQOsGVbTNWeeUKgodea', 'BEED', 3, 'orakol@gmail.com', 'ormoc', 'IMG_1234.jpg', 30),
+(12345, 'Sermonia', 'John Carlo ', 'Dominguez', '$2y$10$0W6a/c6UxghdaNhSF7P0F.d16Qdqtz5c4TzWFSsLhfgFkcCpWhVK6', 'BSIT', 3, 'carlosermonia14@gmail.com', 'Lapu Lapu', 'IMG_Default.jpg', 30),
+(123456, 'Carlo', 'Sermonia', 'Dominguez', '$2y$10$qtYgEBe0P38jZaivahNDje3gUm/fhV1LnkZMze1me57S0Y0DqYwG6', 'BSED', 3, 'Carlo123@gmail.com', 'Suba, Cebu City', 'IMG_Default.jpg', 29),
+(23792344, 'Aranez', 'AxelJake', '', '$2y$10$Qvs.CxpVDe4QyN8vcas/FuUl56KALBLLmBfDuHdl1b0WBzuVGjcq2', 'BSIT', 3, 'axeljake@gmail.com', 'lahug', 'IMG_Default.jpg', 30),
+(23798606, 'Ocarol', 'Leandro', 'Cabalda', '$2y$10$hPMpSW2uG8xLSR/D60ALFOo0dE3Uv2UXr4dWb6PrneeTdzHdq2zSy', 'BSIT', 3, 'ocaroleandro@gmail.com', 'Pasil, Cebu City', 'IMG_23798606.jpg', 28);
 
 --
 -- Indexes for dumped tables
